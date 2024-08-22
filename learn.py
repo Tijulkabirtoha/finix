6b7009d4b26bfa07ec9c27828e2ee8e04b4907e9
Tijulkabirtoha <tijulkabir02@gmail.com>
2024-08-22 20:37:42 +0600

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,8 @@
+temperature = input("Enter today,s temperature :")
+if (int(temperature))>30:
+    print("Weather is hot")
+elif (int(temperature))<=30 and (int(temperature))>20:
+    print("Sunny day")
+else:
+    (int(temperature))<20
+    print("Cold")
